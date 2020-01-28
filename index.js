a2ba36630fc95475dd32ec3b289f9d74a715b505
villarreal1 <villarreal1@email.arizona.edu>
2020-01-28 12:23:25 -0700

    Added some basic structure to what the page should look like manually

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+Author: Alberto Villarreal
+Summary:
+	Insert summary here
+*/
+"use strict";
+(function() {
+   
+   //Function to load stuff onto webpage
+   window.onload = function(){
+      
+   };
+
+})();
